ae8fe060f6b8123e14e72d8cba9eeb0cb0e9df46
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-06-29 07:46:43 +0700

    Update and rename yinscollab.py to albycollab.py

--- AyiinXd/modules/albycollab.py
+++ AyiinXd/modules/albycollab.py
@@ -211,7 +211,7 @@ async def _(asupng):
 
 CMD_HELP.update(
     {
-        "yinscollab": f"**Plugin:** `yinscollab`\
+        "albycollab": f"**Plugin:** `albycollab`\
         \n\n  »  **Perintah :** `{cmd}exo`\
         \n  »  **Kegunaan :** Untuk Mendapatkan Link Bokp Dari Ch Exorcist.\
         \n\n  »  **Perintah :** `{cmd}as`\
@@ -223,9 +223,9 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "yinsexo": f"**Plugin : **`yinsexo`\
+        "albyexo": f"**Plugin : **`albyexo`\
         \n\n  »  **Perintah:** `Ini Khusus Admin Exorcist Tod Bukan Publik.`\
-        \n  »  **Silahkan Ketik** `{cmd}help yinscollab` **Untuk Mendapatkan Konten.**\
+        \n  »  **Silahkan Ketik** `{cmd}help albycollab` **Untuk Mendapatkan Konten.**\
     "
     }
 )
