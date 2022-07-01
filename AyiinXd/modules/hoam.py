5d4b49fafc8b990182ba0bd963914670b1e4cfb3
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-07-01 23:29:38 +0700

    Create hoam.py

---
+++ AyiinXd/modules/hoam.py
@@ -0,0 +1,189 @@
+# © @punya_alby
+# ⚠️ Don't Remove Credits ⚠️
+# FROM ALBY-Userbot <https://github.com/PunyaAlby/ALBY-Userbot>
+# ALBY GANTENG
+# Hayoooloh Kesini pasti nyari cmd help nya kan? Awokawok yaudah pakai aja
+# ░█████╗░██╗░░░░░██████╗░██╗░░░██╗
+# ██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝
+# ███████║██║░░░░░██████╦╝░╚████╔╝░
+# ██╔══██║██║░░░░░██╔══██╗░░╚██╔╝░░
+# ██║░░██║███████╗██████╦╝░░░██║░░░
+# ╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░
+# PERSETAN DENGAN ORANG YANG HAPUS CREDIT!
+# ⚠️ Do not remove credits ⚠️
+
+from userbot import CMD_HELP
+
+CMD_HELP.update(
+    {
+        "animasigambar": f"❖ **Plugin : **`animasigambar`\
+        \n\n ┌❖ **Perintah :** `.thanks`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tulisan Thanks You.\
+        \n\n ┌❖ **Perintah :** `.malam`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tulisan Selamat Malam.\
+        \n\n ┌❖ **Perintah :** `.rumah`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Rumah.\
+        \n\n ┌❖ **Perintah :** `.join`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Perintah untuk join.\
+        \n\n ┌❖ **Perintah :** `.tmo`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Perintah untuk join TMO.\
+        \n\n ┌❖ **Perintah :** `.ch`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Perintah untuk join Channel.\
+        \n\n ┌❖ **Perintah :** `.tembak`\
+        \n └❖ **Berfungsi : **Mengirim Gambar maksa jadi pacar.\
+        \n\n ┌❖ **Perintah :** `.begal`\
+        \n └❖ **Berfungsi : **Mengirim Gambar untuk begal orang.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar2": f"❖ **Plugin : **`animasigambar2`\
+        \n\n ┌❖ **Perintah :** `.lari`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Sedang Kabur.\
+        \n\n ┌❖ **Perintah :** `.lari2`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Sedang Kabur versi 2.\
+        \n\n ┌❖ **Perintah :** `.lari3`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Sedang Kabur versi 3.\
+        \n\n ┌❖ **Perintah :** `.lari4`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Sedang Kabur versi 4.\
+        \n\n ┌❖ **Perintah :** `.lari5`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Sedang Kabur versi 5.\
+        \n\n ┌❖ **Perintah :** `.lari6`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Sedang Kabur versi 6.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar3": f"❖ **Plugin : **`animasigambar3`\
+        \n\n ┌❖ **Perintah :** `.truk`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Truk.\
+        \n\n ┌❖ **Perintah :** `.kingkong`\
+        \n └❖ **Berfungsi : **Mengirim Gambar wajah Kingkong.\
+        \n\n ┌❖ **Perintah :** `.hmm`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Anjing sedang Curiga.\
+        \n\n ┌❖ **Perintah :** `.wlee`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Anjing sedang Melet.\
+        \n\n ┌❖ **Perintah :** `.penis`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Penis.\
+        \n\n ┌❖ **Perintah :** `.penis2`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Penis versi 2.\
+        \n\n ┌❖ **Perintah :** `.ahhh`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Wanita.\
+        \n\n ┌❖ **Perintah :** `.oke`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Oke.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar4": f"❖ **Plugin : **`animasigambar4`\
+        \n\n ┌❖ **Perintah :** `.sip`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Emoji Like.\
+        \n\n ┌❖ **Perintah :** `.tengkorak`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tengkorak.\
+        \n\n ┌❖ **Perintah :** `.punten`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Punten.\
+        \n\n ┌❖ **Perintah :** `.pantau`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Pantau.\
+        \n\n ┌❖ **Perintah :** `.ahhh`\
+        \n └❖ **Berfungsi : **Mengirim Gambar wanita.\
+        \n\n ┌❖ **Perintah :** `.idiot`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ngatain idiot versi Inggris.\
+        \n\n ┌❖ **Perintah :** `.bau`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ngatain bau.\
+        \n\n ┌❖ **Perintah :** `.gay`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ngatain gay.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar5": f"❖ **Plugin : **`animasigambar5`\
+        \n\n ┌❖ **Perintah :** `.manis`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Kamu manis.\
+        \n\n ┌❖ **Perintah :** `.bot`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Robot.\
+        \n\n ┌❖ **Perintah :** `.hey`\
+        \n └❖ **Berfungsi : **Mengirim Gambar hey hey.\
+        \n\n ┌❖ **Perintah :** `.hallo`\
+        \n └❖ **Berfungsi : **Mengirim Gambar hallo.\
+        \n\n ┌❖ **Perintah :** `.sayhi`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Hi versi emoji.\
+        \n\n ┌❖ **Perintah :** `.fail`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tulisan fail.\
+        \n\n ┌❖ **Perintah :** `.lol`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tulisan lol.\
+        \n\n ┌❖ **Perintah :** `.lool`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tulisan lol didalam badan hewan.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar6": f"❖ **Plugin : **`animasigambar6`\
+        \n\n ┌❖ **Perintah :** `.rawr`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Rawr.\
+        \n\n ┌❖ **Perintah :** `.helikopter`\
+        \n └❖ **Berfungsi : **Mengirim Gambar setelah turun dari helikopter.\
+        \n\n ┌❖ **Perintah :** `.helikopter2`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ingin naik ke helikopter.\
+        \n\n ┌❖ **Perintah :** `.helikopter3`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ngajakin naik helikopter.\
+        \n\n ┌❖ **Perintah :** `.tank`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Tank.\
+        \n\n ┌❖ **Perintah :** `.bundir`\
+        \n └❖ **Berfungsi : **Mengirim Gambar bundir dadah semuanyaa.\
+        \n\n ┌❖ **Perintah :** `.bundir2`\
+        \n └❖ **Berfungsi : **Mengirim Gambar bundir pasrah.\
+        \n\n ┌❖ **Perintah :** `.bundir3`\
+        \n └❖ **Berfungsi : **Mengirim Gambar bundir Gak Punya Ayang.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar7": f"❖ **Plugin : **`animasigambar7`\
+        \n\n ┌❖ **Perintah :** `.ular`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ular.\
+        \n\n ┌❖ **Perintah :** `.ular2`\
+        \n └❖ **Berfungsi : **Mengirim Gambar ular versi 2.\
+        \n\n ┌❖ **Perintah :** `.ular3`\
+        \n └❖ **Berfungsi : **Mengirim animasi hewan ular.\
+        \n\n ┌❖ **Perintah :** `.babi`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Babi.\
+        \n\n ┌❖ **Perintah :** `.anjing`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Anjing.\
+        \n\n ┌❖ **Perintah :** `.musang`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Musang.\
+        \n\n ┌❖ **Perintah :** `.gajah`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Gajah.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "animasigambar8": f"❖ **Plugin : **`animasigambar8`\
+        \n\n ┌❖ **Perintah :** `.spongebob`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Spongebob.\
+        \n\n ┌❖ **Perintah :** `.spongebob2`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Spongebob ngajak ke ladang ubur ubur.\
+        \n\n ┌❖ **Perintah :** `.spongebob3`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Spongebob Ngajak main sama Patrick.\
+        \n\n ┌❖ **Perintah :** `.spongebob4`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Spongebob Ngajak ke Krusty Krab.\
+        \n\n ┌❖ **Perintah :** `.lonte`\
+        \n └❖ **Berfungsi : **Mengirim Gambar Lonte.\
+        \n\n ┌❖ **Perintah :** `.loveyou`\
+        \n └❖ **Berfungsi : **Mengirim Gambar I LOVE YOU.\
+    "
+    }
+)
