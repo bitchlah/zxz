1c942faa556cb555b0639c649f44338965e6c737
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-07-01 07:53:03 +0700

    Create albycouple2.py

---
+++ AyiinXd/modules/albycouple2.py
@@ -0,0 +1,83 @@
+# inspiration from @greyyvbss
+# modification of @punya_alby
+
+from AyiinXd import CMD_HANDLER as cmd
+from AyiinXd import CMD_HELP
+from AyiinXd.ayiin import ayiin_cmd
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterPhotos
+
+
+@ayiin_cmd(pattern="ppcp$")
+async def _(event):
+    try:
+        fotbarnya = [
+            fotbar
+            async for fotbar in event.client.iter_messages(
+                "@fotbarlyorax", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(fotbarnya),
+            caption=f" Nih Kak PP Couple nya 😎 [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("PPCP nya Gada Karena Kamu Jelek _-.")
+
+
+@ayiin_cmd(pattern="ppcp2$")
+async def _(event):
+    try:
+        fotbarnya = [
+            fotbar
+            async for fotbar in event.client.iter_messages(
+                "@fotbarpi", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(fotbarnya),
+            caption=f" Nih Kak PP Couple nya 😎 [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("PPCP nya Gada Karena Kamu Jelek _-.")
+
+
+@ayiin_cmd(pattern="ppcp3$")
+async def _(event):
+    try:
+        fotbarnya = [
+            fotbar
+            async for fotbar in event.client.iter_messages(
+                "@rp_fotbar", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(fotbarnya),
+            caption=f" Nih Kak PP Couple nya 😎 [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("PPCP nya Gada Karena Kamu Jelek _-.")
+
+
+CMD_HELP.update(
+    {
+        "albyppcouple": f"❖ **Plugin : **`ppcouple`\
+        \n\n ┌❖ **Perintah :** `.ppcp`\
+        \n └❖ **Berfungsi : **Untuk Mencari PP Couple secara random.\
+        \n\n ┌❖ **Perintah :** `.ppcp2`\
+        \n └❖ **Berfungsi : **Untuk Mencari PP Couple secara random versi kedua.\
+        \n\n ┌❖ **Perintah :** `.ppcp3`\
+        \n └❖ **Berfungsi : **Untuk Mencari PP Couple secara random versi ketiga.\
+    "
+    }
+)
